fc3df5678bafb4f780695dfed8a214b80fa70d39
selma_bettaieb <sbettaie56@gmail.com>
2025-03-06 14:50:28 +0100

    fixed layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        teal: '#2B6A6E',
-        coral: '#FF6F61',
-        slate: '#2D3748',
-        lightSlate: '#A0AEC0',
-      },
-      animation: {
-        'fade-in': 'fadeIn 0.5s ease-in',
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          teal: '#2B6A6E',
+          coral: '#FF6F61',
+          slate: '#2D3748',
+          lightSlate: '#A0AEC0',
+        },
+        animation: {
+          'fade-in': 'fadeIn 0.5s ease-in',
+        },
+        keyframes: {
+          fadeIn: {
+            '0%': { opacity: '0' },
+            '100%': { opacity: '1' },
+          },
         },
       },
     },
-  },
-  plugins: [],
-} 
+    plugins: [],
+  }
