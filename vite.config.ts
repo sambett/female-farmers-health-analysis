48effe23923dee2213b2c9a41479ff3ab6a37ac7
selma_bettaieb <sbettaie56@gmail.com>
2025-04-02 05:15:24 +0100

    fixed nlp so far but still empty page

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@emotion/react': path.resolve(__dirname, 'node_modules/@emotion/react'),
+      '@emotion/styled': path.resolve(__dirname, 'node_modules/@emotion/styled'),
+      '@emotion/cache': path.resolve(__dirname, 'node_modules/@emotion/cache')
+    },
+  },
   build: {
     outDir: 'dist',
     rollupOptions: {
@@ -16,5 +25,8 @@ export default defineConfig({
         }
       }
     }
+  },
+  optimizeDeps: {
+    include: ['@emotion/react', '@emotion/styled', '@emotion/cache']
   }
 })
